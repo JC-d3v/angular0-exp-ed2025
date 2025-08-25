1eb6317beade3a7dfb9aef71b5797b13ab119bc3
Jorge Calderon <calderon.rjorge@gmail.com>
2025-08-25 15:48:20 -0700

    S3 Funciones con objetos como argumentos

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 
 // import './topics/01-baisc-types'
-import './topics/02-object-interfaces'
+// import './topics/02-object-interfaces'
+import './topics/03-functions'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
